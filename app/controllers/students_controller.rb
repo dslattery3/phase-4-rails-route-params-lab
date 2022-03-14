5659349a61c4e8be036a442b5ed49c228db7ff99
Dalton <slattery.da@gmail.com>
2022-03-14 17:07:52 -0500

    commit tree aab48ce2bc7f0fdcdaf406f3e6bf3827eaacf2e2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,25 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    name = params[:name]
+    if (!!name == false)
+      students = Student.all
+      render json: students
+    else
+      first_match = Student.all.where({first_name: name.capitalize})
+      last_match = Student.all.where({last_name: name.capitalize})
+      if (first_match.size > 0 )
+        render json: first_match
+      elsif (last_match.size > 0)
+        render json: last_match
+      end
+    end    
   end
+
+  def show
+    student = Student.all.find(params[:id])
+    render json: student
+  end
+
 
 end
